30e93f21be9548f48b4625e5b0bf30f5a1730924
Somhrita <joardarsomhrita@gmail.com>
2025-06-28 16:54:50 +0530

    added register login landing dashboard pages

--- Clint/src/App.jsx
+++ Clint/src/App.jsx
@@ -1,13 +1,23 @@
 import { useState } from 'react'
 import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Landing from './pages/Landing'
+import Login from './pages/login'
+import Register from './pages/Register'
+import Dashboard from './pages/Dashboard'
 
 function App() {
-
   return (
-    <>
-     hello world
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} /> 
+        <Route path="/dashboard" element={<Dashboard />} /> 
+      </Routes>
+    </Router>
   )
 }
+
 
 export default App
